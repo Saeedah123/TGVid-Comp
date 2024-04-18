0438118040ea38bea78e619f1da5616776b7e538
Saeedah123 <149072361+Saeedah123@users.noreply.github.com>
2024-04-18 07:19:07 +0500

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -22,25 +22,25 @@ async def get_stats(bot, message):
     uptime = time.strftime("%Hh%Mm%Ss", time.gmtime(
         time.time() - Config.BOT_UPTIME))
     start_t = time.time()
-    st = await message.reply('**Aᴄᴄᴇꜱꜱɪɴɢ Tʜᴇ Dᴇᴛᴀɪʟꜱ.....**')
+    st = await message.reply('**Accessing The Details.....**')
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await st.edit(text=f"**--Bᴏᴛ Sᴛᴀᴛᴜꜱ--** \n\n**⌚️ Bᴏᴛ Uᴩᴛɪᴍᴇ:** {uptime} \n**🐌 Cᴜʀʀᴇɴᴛ Pɪɴɢ:** `{time_taken_s:.3f} ᴍꜱ` \n**👭 Tᴏᴛᴀʟ Uꜱᴇʀꜱ:** `{total_users}`")
+    await st.edit(text=f"**--Bot Status--** \n\n**⌚️ Bot Uptime:** {uptime} \n**🐌 Current Ping:** `{time_taken_s:.3f} ms` \n**👭 Total Users:** `{total_users}`")
 
 
 # Restart to cancell all process
 @Client.on_message(filters.private & filters.command("restart") & filters.user(Config.ADMIN))
 async def restart_bot(b, m):
-    await m.reply_text("🔄__Rᴇꜱᴛᴀʀᴛɪɴɢ.....__")
+    await m.reply_text("🔄__Restarting.....__")
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
 @Client.on_message(filters.command("broadcast") & filters.user(Config.ADMIN) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
-    await bot.send_message(Config.LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} Iꜱ ꜱᴛᴀʀᴛᴇᴅ ᴛʜᴇ Bʀᴏᴀᴅᴄᴀꜱᴛ......")
+    await bot.send_message(Config.LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} Is Started The Broadcast......")
     all_users = await db.get_all_users()
     broadcast_msg = m.reply_to_message
-    sts_msg = await m.reply_text("Bʀᴏᴀᴅᴄᴀꜱᴛ Sᴛᴀʀᴛᴇᴅ..!")
+    sts_msg = await m.reply_text("Broadcast Started..!")
     done = 0
     failed = 0
     success = 0
@@ -56,9 +56,9 @@ async def broadcast_handler(bot: Client, m: Message):
             await db.delete_user(user['id'])
         done += 1
         if not done % 20:
-            await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Iɴ Pʀᴏɢʀᴇꜱꜱ: \nTᴏᴛᴀʟ Uꜱᴇʀꜱ {total_users} \nCᴏᴍᴩʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nFᴀɪʟᴇᴅ: {failed}")
+            await sts_msg.edit(f"Broadcast In Progress: \nTotal Users {total_users} \nCompleted: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nFailed: {failed}")
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
-    await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Cᴏᴍᴩʟᴇᴛᴇᴅ: \nCᴏᴍᴩʟᴇᴛᴇᴅ Iɴ `{completed_in}`.\n\nTᴏᴛᴀʟ Uꜱᴇʀꜱ {total_users}\nCᴏᴍᴩʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nFᴀɪʟᴇᴅ: {failed}")
+    await sts_msg.edit(f"Broadcast Completed: \nCompleted In `{completed_in}`.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nFailed: {failed}")
 
 
 async def send_msg(user_id, message):
@@ -69,13 +69,13 @@ async def send_msg(user_id, message):
         await asyncio.sleep(e.value)
         return send_msg(user_id, message)
     except InputUserDeactivated:
-        logger.info(f"{user_id} : Dᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ")
+        logger.info(f"{user_id} : Deactivated")
         return 400
     except UserIsBlocked:
-        logger.info(f"{user_id} : Bʟᴏᴄᴋᴇᴅ Tʜᴇ Bᴏᴛ")
+        logger.info(f"{user_id} : Blocked The Bot")
         return 400
     except PeerIdInvalid:
-        logger.info(f"{user_id} : Uꜱᴇʀ Iᴅ Iɴᴠᴀʟɪᴅ")
+        logger.info(f"{user_id} : User Id Invalid")
         return 400
     except Exception as e:
         logger.error(f"{user_id} : {e}")
@@ -87,7 +87,7 @@ async def ban(c: Client, m: Message):
 
     if len(m.command) == 1:
         await m.reply_text(
-            f"Use this command to ban any user from the bot.\n\n"
+            f"Use This Command To Ban Any User From The Bot.\n\n"
             f"Usage:\n\n"
             f"`/ban_user user_id ban_duration ban_reason`\n\n"
             f"Eg: `/ban_user 1234567 28 You misused me.`\n"
@@ -100,17 +100,17 @@ async def ban(c: Client, m: Message):
         user_id = int(m.command[1])
         ban_duration = int(m.command[2])
         ban_reason = ' '.join(m.command[3:])
-        ban_log_text = f"Banning user {user_id} for {ban_duration} days for the reason {ban_reason}."
+        ban_log_text = f"Banning User {user_id} For {ban_duration} Days For The Reason {ban_reason}."
         try:
             await c.send_message(
                 user_id,
-                f"You are banned to use this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__ \n\n"
-                f"**Message from the admin**"
+                f"You Are Banned To Use This Bot For **{ban_duration}** day(s) For The Reason __{ban_reason}__ \n\n"
+                f"**Message From The Admin**"
             )
-            ban_log_text += '\n\nUser notified successfully!'
+            ban_log_text += '\n\nUser Notified Successfully!'
         except:
             traceback.print_exc()
-            ban_log_text += f"\n\nUser notification failed! \n\n`{traceback.format_exc()}`"
+            ban_log_text += f"\n\nUser Notification Failed! \n\n`{traceback.format_exc()}`"
 
         await db.ban_user(user_id, ban_duration, ban_reason)
         print(ban_log_text)
@@ -121,7 +121,7 @@ async def ban(c: Client, m: Message):
     except:
         traceback.print_exc()
         await m.reply_text(
-            f"Error occoured! Traceback given below\n\n`{traceback.format_exc()}`",
+            f"Error Occoured! Traceback Given Below\n\n`{traceback.format_exc()}`",
             quote=True
         )
 
@@ -131,23 +131,23 @@ async def unban(c: Client, m: Message):
 
     if len(m.command) == 1:
         await m.reply_text(
-            f"Use this command to unban any user.\n\n"
+            f"Use This Command To Unban Any User.\n\n"
             f"Usage:\n\n`/unban_user user_id`\n\n"
             f"Eg: `/unban_user 1234567`\n"
-            f"This will unban user with id `1234567`.",
+            f"This Will Unban User With Id `1234567`.",
             quote=True
         )
         return
 
     try:
         user_id = int(m.command[1])
-        unban_log_text = f"Unbanning user {user_id}"
+        unban_log_text = f"Unbanning User {user_id}"
         try:
             await c.send_message(
                 user_id,
-                f"Your ban was lifted!"
+                f"Your Ban Was Lifted!"
             )
-            unban_log_text += '\n\nUser notified successfully!'
+            unban_log_text += '\n\nUser Notified Successfully!'
         except:
             traceback.print_exc()
             unban_log_text += f"\n\nUser notification failed! \n\n`{traceback.format_exc()}`"
@@ -160,7 +160,7 @@ async def unban(c: Client, m: Message):
     except:
         traceback.print_exc()
         await m.reply_text(
-            f"Error occurred! Traceback given below\n\n`{traceback.format_exc()}`",
+            f"Error Occurred! Traceback Given Below\n\n`{traceback.format_exc()}`",
             quote=True
         )
 
@@ -180,7 +180,7 @@ async def _banned_users(_, m: Message):
         banned_usr_count += 1
         text += f"> **user_id**: `{user_id}`, **Ban Duration**: `{ban_duration}`, " \
                 f"**Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
-    reply_text = f"Total banned user(s): `{banned_usr_count}`\n\n{text}"
+    reply_text = f"Total Banned User(s): `{banned_usr_count}`\n\n{text}"
     if len(reply_text) > 4096:
         with open('banned-users.txt', 'w') as f:
             f.write(reply_text)
@@ -188,3 +188,4 @@ async def _banned_users(_, m: Message):
         os.remove('banned-users.txt')
         return
     await m.reply_text(reply_text, True)
+    
