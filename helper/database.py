6803e28c0b07be8f08b081253a667d6d5493449e
Saeedah123 <149072361+Saeedah123@users.noreply.github.com>
2024-04-18 07:22:36 +0500

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -17,7 +17,7 @@ class Database:
             caption=None,
             thumbnail=None,
             ffmpegcode=None,
-            metadata=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Kdramaland" -metadata author="@Snowball_Official" -metadata:s:s title="Subtitled By :- @Kdramaland" -metadata:s:a title="By :- @Kdramaland" -metadata:s:v title="By:- @Snowball_Official" """,
+            metadata=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @KingBjss" -metadata author="@KingBjssChat" -metadata:s:s title="Subtitled By :- @KingBjss" -metadata:s:a title="By :- @KingBjss" -metadata:s:v title="By:- @KingBjssChat" """,
             ban_status=dict(
                 is_banned=False,
                 ban_duration=0,
